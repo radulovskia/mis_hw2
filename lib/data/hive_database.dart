c492f2d983b1dfb70c97da762cda33ec4a4b50c4
radulovskia <85944330+somefinkistudent@users.noreply.github.com>
2024-02-02 16:25:05 +0100

    Add workout and exercise conversion methods to store into hive

--- lib/data/hive_database.dart
+++ lib/data/hive_database.dart
@@ -1,3 +1,39 @@
-class HiveDatabase {
+import '../models/exercise.dart';
+import '../models/workout.dart';
 
+class HiveDatabase {}
+
+List<String> convertObjectToWorkoutList(List<Workout> workouts) {
+  List<String> workoutList = [];
+
+  for (int i = 0; i < workouts.length; i++) {
+    workoutList.add(workouts[i].name);
+  }
+
+  return workoutList;
+}
+
+List<List<List<String>>> convertObjectToExerciseList(List<Workout> workouts) {
+  List<List<List<String>>> exerciseList = [];
+
+  for (int i = 0; i < workouts.length; i++) {
+    List<Exercise> exercisesInWorkout = workouts[i].exercises;
+    List<List<String>> individualWorkout = [];
+
+    for (int j = 0; j < exercisesInWorkout.length; j++) {
+      List<String> individualExercise = [];
+
+      individualExercise.addAll([
+        exercisesInWorkout[j].name,
+        exercisesInWorkout[j].weight,
+        exercisesInWorkout[j].reps,
+        exercisesInWorkout[j].sets,
+        exercisesInWorkout[j].isCompleted.toString(),
+      ]);
+      individualWorkout.add(individualExercise);
+    }
+
+    exerciseList.add(individualWorkout);
+  }
+  return exerciseList;
 }
