9f1a7eb227aabb7be13e0f438950536957403099
radulovskia <85944330+somefinkistudent@users.noreply.github.com>
2024-02-02 16:16:48 +0100

    Add hive for local storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:hw2/data/workout_data.dart';
 import 'package:provider/provider.dart';
 import 'pages/home_page.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  await Hive.openBox("workout_database");
   runApp(const MyApp());
 }
 
