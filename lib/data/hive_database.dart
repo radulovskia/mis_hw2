2e9da79c7badf51de98ee404120729849fd9b4eb
radulovskia <85944330+somefinkistudent@users.noreply.github.com>
2024-02-02 16:54:28 +0100

    Add Hive methods

--- lib/data/hive_database.dart
+++ lib/data/hive_database.dart
@@ -1,7 +1,85 @@
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:hw2/datetime/date_time.dart';
+
 import '../models/exercise.dart';
 import '../models/workout.dart';
 
-class HiveDatabase {}
+class HiveDatabase {
+  final _myBox = Hive.box("workout_database");
+
+  bool previousDataExists() {
+    if (_myBox.isEmpty) {
+      print("previous data does NOT exist");
+      _myBox.put("START_DATE", todaysDateYYYYMMDD());
+      return false;
+    } else {
+      print("previous data DOES exist");
+      return true;
+    }
+  }
+
+  String getStartDate() {
+    return _myBox.get("START_DATE");
+  }
+
+  bool exerciseCompleted(List<Workout> workouts) {
+    for (var w in workouts) {
+      for (var e in w.exercises) {
+        if (e.isCompleted) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  void saveToDatabase(List<Workout> workouts) {
+    final workoutList = convertObjectToWorkoutList(workouts);
+    final exerciseList = convertObjectToExerciseList(workouts);
+
+    if (exerciseCompleted(workouts)) {
+      // 1 means completed, 0 means not completed
+      _myBox.put("COMPLETION_STATUS_${todaysDateYYYYMMDD()}", 1);
+    } else {
+      _myBox.put("COMPLETION_STATUS_${todaysDateYYYYMMDD()}", 0);
+    }
+
+    _myBox.put("WORKOUTS", workoutList);
+    _myBox.put("EXERCISES", exerciseList);
+  }
+
+  List<Workout> readFromDatabase() {
+    List<Workout> mySavedWorkouts = [];
+    List<String> workoutNames = _myBox.get("WORKOUTS");
+    final exerciseDetails = _myBox.get("EXERCISES");
+
+    for (int i = 0; i < workoutNames.length; i++) {
+      List<Exercise> exercisesInEachWorkout = [];
+
+      for (int j = 0; j < exerciseDetails[i].length; j++) {
+        Exercise e = Exercise(
+            name: exerciseDetails[i][j][0],
+            weight: exerciseDetails[i][j][1],
+            reps: exerciseDetails[i][j][2],
+            sets: exerciseDetails[i][j][3]);
+        exerciseDetails[i][j][4] == "true"
+            ? e.isCompleted = true
+            : e.isCompleted = false;
+        exercisesInEachWorkout.add(e);
+      }
+
+      Workout w =
+          Workout(name: workoutNames[i], exercises: exercisesInEachWorkout);
+      mySavedWorkouts.add(w);
+    }
+
+    return mySavedWorkouts;
+  }
+
+  int getCompletedStatus(String yyyymmdd) {
+    return _myBox.get("COMPLETION_STATUS_$yyyymmdd") ?? 0;
+  }
+}
 
 List<String> convertObjectToWorkoutList(List<Workout> workouts) {
   List<String> workoutList = [];
